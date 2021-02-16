2977388ddd69e4067d502b8540728ba48b731ac3
Jacob Schramkowski <54072198+jschramk@users.noreply.github.com>
2021-02-16 01:05:38 -0600

    changed PID interface
    added methods for setting target points and checking for when the target is reached under defined conditions

--- PIDController.java
+++ PIDController.java
@@ -1,11 +1,10 @@
-// add your package name below and uncomment
-//package <your package name>
+package pid;
 
 /**
  * @author Jacob Schramkowski
- *
+ * <p>
  * A simple implementation of a Proportional-Integral-Derivative (PID) Controller
- * designed for single-line use
+ * designed for drop-in use
  */
 public class PIDController {
 
@@ -17,11 +16,20 @@ public class PIDController {
   private double integral = 0;
   private long prevTime = System.nanoTime();
 
+  // field for set end point
+  private double target = 0;
+
+  // fields for detecting when the object is at its target position
+  private double stopThreshold = 1;
+  private int stopCount = 10;
+  private int currCount = 0;
+
   // optional min and max output values, default to NaN if unused
   private double min = Double.NaN, max = Double.NaN;
 
   /**
    * Constructor for PID Controller object
+   *
    * @param Kp the proportional control factor
    * @param Ki the integral control factor
    * @param Kd the derivative control factor
@@ -35,13 +43,16 @@ public class PIDController {
   /**
    * Gets the value of the control output given the distance from the current
    * position to the desired position
-   * @param error the signed distance from the current to desired position
-   * @return the control output
+   *
+   * @param position the signed current position of the controlled object
+   * @return the control output value
    */
-  public double getValue(double error) {
+  public double getControlOutput(double position) {
+
+    double error = target - position;
 
     // compute change in time since last getValue() call in seconds
-    double timeDelta = (System.nanoTime() - prevTime)/1e9;
+    double timeDelta = (System.nanoTime() - prevTime) / 1e9;
 
     // add integral error
     integral += prevError * timeDelta;
@@ -62,6 +73,14 @@ public class PIDController {
       value = Math.min(value, max);
     }
 
+    // if the absolute value of the error is within the acceptable threshold,
+    // increment the count, else reset it
+    if (Math.abs(error) < stopThreshold) {
+      currCount++;
+    } else {
+      currCount = 0;
+    }
+
     // set the previous error field equal to the current error for the next loop
     prevError = error;
 
@@ -73,7 +92,50 @@ public class PIDController {
   }
 
   /**
+   * Sets the desired end point for the controlled object
+   *
+   * @param target the target point in the same units as the value
+   *               fed into getControlOutput()
+   */
+  public void setTarget(double target) {
+    this.target = target;
+    currCount = 0;
+  }
+
+  /**
+   * Gets the currently set target point
+   *
+   * @return the target point
+   */
+  public double getTarget() {
+    return target;
+  }
+
+  /**
+   * Checks whether the controlled object is at its desired end point
+   *
+   * @return true if the object has been within an acceptable range
+   * for a specified number of consecutive cycles
+   */
+  public boolean atTarget() {
+    return currCount > stopCount;
+  }
+
+  /**
+   * Sets the conditions for when atTarget() returns true
+   *
+   * @param threshold the acceptable error above or below the target point
+   * @param count     the number of consecutive times the position must fall within the
+   *                  above range
+   */
+  public void setTargetDetection(double threshold, int count) {
+    this.stopThreshold = threshold;
+    this.stopCount = count;
+  }
+
+  /**
    * Sets the minimum control output value
+   *
    * @param min the minimum value of the control output
    */
   public void setMin(double min) {
@@ -82,6 +144,7 @@ public class PIDController {
 
   /**
    * Sets the maximum control output value
+   *
    * @param max the maximum value of the control output
    */
   public void setMax(double max) {
